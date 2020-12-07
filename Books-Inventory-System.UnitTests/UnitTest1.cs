4c20b82e2bd7983441101e48265f458bcb267f2c
Aneeq Anwar <aneeq.anwar@confiz.com>
2020-12-08 03:42:09 +0500

    Added unit tests project

---
+++ Books-Inventory-System.UnitTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using Books_Inventory_System.Models;
+using NUnit.Framework;
+
+namespace Books_Inventory_System.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void BookAuthorNameTest()
+        {
+            Book book = new Book();
+            Assert.That(book.Author, Is.EqualTo("Seth Godin"));
+        }
+    }
+}
